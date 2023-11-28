a9321185a40146d3c5bc23a75c4673028f4bc3b4
Franklin Tochukwu <tchfrnkln@gmail.com>
2023-11-28 03:12:47 +0100

    initail commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className={`flex-1 justify-center items-center`} >
+      <Text className="text-green-700">Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Authentication({navigation}) {
+  return (
+    <View className={`flex-1 justify-center items-center`}>
+      <Text onPress={e => navigation.push("Home")}>Authentication</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Authentication'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Authentication" component={Authentication} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
